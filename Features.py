8c354b258ccf2e06f5759047bc92612a4ac28e56
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-10-02 03:23:49 +0530

    focus graph feature

--- Features.py
+++ Features.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as pt
 import pyttsx3
 import datetime
 import speech_recognition
@@ -341,3 +342,22 @@ def game_play():
         i += 1
 
     print(f"FINAL SCORE :- ME :- {Me_score} : COM :- {Com_score}")
+
+
+def focus_graph():
+    file = open("focus.txt", "r")
+    content = file.read()
+    file.close()
+    content = content.split(",")
+    x1 = []
+    for i in range(0, len(content)):
+        content[i] = float(content[i])
+        x1.append(i)
+    print(content)
+    y1 = content
+    pt.plot(x1, y1, color="red", marker="o")
+    pt.title("YOUR FOCUSED TIME", fontsize=16)
+    pt.xlabel("Times", fontsize=14)
+    pt.ylabel("Focus Time", fontsize=14)
+    pt.grid()
+    pt.show()
