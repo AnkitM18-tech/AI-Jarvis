f8203a5ef6e2fc31ddd3b492846391ea5fd92130
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-10-01 18:43:07 +0530

    news function

--- Features.py
+++ Features.py
@@ -8,6 +8,10 @@ import os
 import pyautogui
 from time import sleep
 from pynput.keyboard import Key, Controller
+import requests
+import json
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def takeCommand():
@@ -179,3 +183,50 @@ def volumedown():
         keyboard.press(Key.media_volume_down)
         keyboard.release(Key.media_volume_down)
         sleep(0.1)
+
+
+def latestnews():
+    API_KEY = os.getenv("NEWS_API_KEY")
+    api_dict = {"business": f"https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey={API_KEY}",
+                "entertainment": f"https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey={API_KEY}",
+                "health": f"https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey={API_KEY}",
+                "science": f"https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey={API_KEY}",
+                "sports": f"https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey={API_KEY}",
+                "technology": f"https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey={API_KEY}"
+                }
+
+    content = None
+    url = None
+    speak(
+        "Which field news do you want, [business] , [health] , [technology], [sports] , [entertainment] , [science]")
+    field = input("Type field news that you want: ")
+    for key, value in api_dict.items():
+        if key.lower() in field.lower():
+            url = value
+            print(url)
+            print("url was found")
+            break
+        else:
+            url = True
+    if url is True:
+        print("url not found")
+
+    news = requests.get(url).text
+    news = json.loads(news)
+    speak("Here is the first news.")
+
+    arts = news["articles"]
+    for articles in arts:
+        article = articles["title"]
+        print(article)
+        speak(article)
+        news_url = articles["url"]
+        print(f"for more info visit: {news_url}")
+
+        a = input("[press 1 to cont] and [press 2 to stop]")
+        if str(a) == "1":
+            pass
+        elif str(a) == "2":
+            break
+
+    speak("thats all")
